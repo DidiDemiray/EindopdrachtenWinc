52dba77d9faa05dd65de53b848d79bf98409eac7
DidiDemiray <64772171+DidiDemiray@users.noreply.github.com>
2020-06-16 16:49:34 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react';
+import './App.css';
+import Chart from './components/Chart'; 
+import ButtonS from './components/buttonClass';
+import EvelynChart from './components/EvelynChart';
+import ArankaChart from './components/ArankaChart';
+import FlorisChart from './components/FlorisChart';
+import HectorChart from './components/HectorChart';
+import MartinaChart from './components/MartinaChart';
+import MauritsChart from './components/MauritsChart';
+import RahimaChart from './components/RahimaChart';
+import SandraChart from './components/SandraChart';
+import WietskeChart from './components/WietskeChart';
+import StormChart from './components/StormChart';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    //Data ophalen, normaal uit API
+    this.setState({
+      chartData:{
+        labels: ['SCRUM','W1D1-1','W1D2-1','W1D2-2',' W1D2-3','W1D2-4','W1D2-5','W1D3-1',
+                  'W1D3-2','W1D3-4','W1D3-5',' W1D3 - Project - Guess-the-number','W1D4-1','W1D4 - Project - Kleurentoggle',
+                  'W1D5 - Project - Galgje','W2D1-1','W2D1-2','W2D2-1','W2D2-2','W2D2-3','W2D3-1','W2D3-2',
+                  'W2D3-3','W2D4-1','W2D4-2','W2D4-3','W2D5 - Project - Filmzoeker','W3D1-1','W3D1-2','W3D1-3',
+                  'W3D1-4','W3D2-1','W3D2-2','W3D2-3','W3D3-1','W3D3-2','W3D3-3','W3D3-4','W3D4-1','W3D4-2','W3D5 - Project - Todo-List',
+                  'W4D2-1','W4D2-2','W4D2-3','W4D2-4','W4D3-1','W4D3-2','W4D3-3','W4D3-4','W4D3-5','W4D3 - Project - Next-Level CSS',
+                  'W5D4-1','W5D5 - Project - Lil_Playlist','W6D1-1','W6D2-1'],
+        datasets: [
+            {
+                label:'LEUK',
+                data:[
+                  2, 3, 3, 2.7, 2.5, 2.4, 3, 2.9, 2.5, 2, 2.6, 2.7, 2.8, 2.9, 3.1, 2.8, 2.4, 2.6, 2.5, 2.5, 2, 
+                  2.6, 2.3, 3.4, 3, 2.5, 2.1, 2.3, 3.1, 2.9, 2.9, 3, 2.9, 2.6, 3, 2.4, 3.1, 2.4, 3.4, 2.7, 3.1,
+                  2.7, 2.8, 3, 3, 2.5, 3.1, 2.7, 2.6, 2.5, 2.8, 2.8, 1.9, 2.6, 2.5, 2.8
+                    ],
+                fill: false,
+                backgroundColor:'blue',
+                borderColor:'blue'
+            },
+            {
+                label:'MOEILIJK',
+                data:[
+                  1.9, 3.1, 2, 1.6, 1.9, 2.6, 2.7, 1.9, 2.2, 2.4, 2.7, 2.5, 2.9, 2.5, 3, 2.2, 3.3, 2.9, 2.3,
+                  2.8, 2.4, 2.4, 2.8, 1.9, 3.1, 2.6, 2.9, 2.4, 2.2, 2.2, 2.2, 1.8, 2.1, 2.5, 2.4, 2.9, 2.3, 2.9,
+                  2.6, 2.2, 2.2, 2.1, 2.3, 2.4, 2.4, 2.2, 1.9, 2.1, 2.1, 2.5, 2.7, 1.9, 2.7, 2.3, 1.9, 3
+                    ],
+                fill: false,
+                backgroundColor:'yellow',
+                borderColor:'yellow'
+
+            }
+        ]
+    }
+    });
+  }
+
+  render (){
+    return(
+      <div className="App">
+      <header className="App-header">
+        <p>
+          STUDENT DASHBOARD
+        </p>
+      
+      </header>
+      
+      <body className='App-body'>
+        
+        <Chart chartData={this.state.chartData} />
+        <ButtonS /> 
+        <br/>
+        <EvelynChart />
+        <ArankaChart />
+        <FlorisChart />
+        <HectorChart />
+        <MartinaChart />
+        <MauritsChart />
+        <RahimaChart />
+        <SandraChart />
+        <WietskeChart />
+        <StormChart />
+      </body>
+
+      <footer className='App-footer'>
+        <p><strong>Didi Demiray</strong></p>
+      </footer>
+    </div>
+    )
+    
+  };
+}
+
+export default App;
---
+++ src/components/buttonClass.js
@@ -0,0 +1,159 @@
+import React, {Component} from 'react';
+
+class ButtonS extends Component{
+  constructor(props){
+    super(props);
+    this.shoot1=this.shoot1.bind(this);
+    this.shoot2=this.shoot2.bind(this);
+    this.shoot3=this.shoot3.bind(this);
+    this.shoot4=this.shoot4.bind(this);
+    this.shoot5=this.shoot5.bind(this);
+    this.shoot6=this.shoot6.bind(this);
+    this.shoot7=this.shoot7.bind(this);
+    this.shoot8=this.shoot8.bind(this);
+    this.shoot9=this.shoot9.bind(this);
+    this.shoot10=this.shoot10.bind(this);
+  }
+
+  shoot1(){
+    document.querySelector('.evelynchart').style.display = 'block';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot2(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'block';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot3(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'block';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot4(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'block';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot5(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'block';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot6(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'block';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot7(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'block';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot8(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'block';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot9(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'block';
+    document.querySelector('.stormchart').style.display = 'none';
+  }
+  shoot10(){
+    document.querySelector('.evelynchart').style.display = 'none';
+    document.querySelector('.arankachart').style.display = 'none';
+    document.querySelector('.florischart').style.display = 'none';
+    document.querySelector('.hectorchart').style.display = 'none';
+    document.querySelector('.martinachart').style.display = 'none';
+    document.querySelector('.mauritschart').style.display = 'none';
+    document.querySelector('.rahimachart').style.display = 'none';
+    document.querySelector('.sandrachart').style.display = 'none';
+    document.querySelector('.wietskechart').style.display = 'none';
+    document.querySelector('.stormchart').style.display = 'block';
+  }
+
+    render(){
+        return(
+        <div>
+        <form>
+          <button onClick={this.shoot1} >Evelyn</button>
+          <button onClick={this.shoot2}>Aranka</button>
+          <button onClick={this.shoot3}>Floris</button>
+          <button onClick={this.shoot4}>Hector</button>
+          <button onClick={this.shoot5}>Martina</button>
+          <button onClick={this.shoot6}>Maurits</button>
+          <button onClick={this.shoot7}>Rahima</button>
+          <button onClick={this.shoot8}>Sandra</button>
+          <button onClick={this.shoot9}>Wietske</button>
+          <button onClick={this.shoot10}>Storm</button>
+        </form>
+        </div>
+        )
+    }
+}
+
+export default ButtonS;
